4a4edcc8b502fc7fd6e0b9a02b32b2eb1039f0aa
tushar1003 <91830386+tushar1003@users.noreply.github.com>
2022-03-13 22:01:44 +0530

    MongoDb

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose=require("mongoose");
+
 const homeStartingContent =
   "Whether you’re looking for a tool to record your daily emotions and activities in a reflective journal, keep track of milestones in a food diary or pregnancy journal, or even record your dreams in a dream journal, Blog-Diary  has you covered.Blog-Diary  gives you all the tools you need to focus on the ideas you want to preserve, rather than the process of writing itself.Even when carefully kept, paper journals can be read by anyone who happens upon them. Blog-Diary keeps your journals safe with double password protection and military strength encryption so you can rest easy knowing that your entries are secure in the Blog-Diary Vault.Take your journals wherever you go with the Blog-Diary  mobile apps for iPhone, iPad, and Android phones and tablets.  ";
 const aboutContent ="Once there was a guy named Bill who wanted a horse. On Craigslist, Bill saw a Christian horse so he went to check it out. When Bill got to the ranch, the horse's owner said .It's easy to ride him. Just say 'praise the Lord' to make him go, and 'amen' to make him stop. Bill got on the horse and said praise the Lord. the horse started to walk. Praise the Lord, praise the Lord, praise the Lord and the horse is running. Now Bill sees the cliff and says: AMEN. The horse stops and Bill says: Whew! Praise the lord!"
@@ -11,48 +13,86 @@ const contactContent =
 "A man came through my lane at the grocery store with a jug of wine and a bouquet of roses. But before paying, he set the two items aside and said, “I’ll be right back.” He ran off, only to ­return a minute later with a second jug of wine and another bouquet of roses. “Two girlfriends?” I asked.“No,” he said. “Just one really angry one.”"
 const port=process.env.PORT || 3000;
 const app = express();
+mongoose.connect("mongodb+srv://admin-tushar:Test123@cluster0.os5nk.mongodb.net/blogDB", {useNewUrlParser: true});
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let arr = [{
-  title:"Day0",
-  content:"Hey Buddy!!!! Don't worry everything is secret with me"
-}];
+// let posts = [];
+
+const postSchema = {
+
+  title: String,
+ 
+  content: String
+ 
+ };
+
+ const Post = mongoose.model("Post", postSchema);
+
+
 
 app.get("/", function (req, res) {
-  res.render("home", { content: homeStartingContent, posts: arr });
+  Post.find({}, function(err, posts){
+
+    res.render("home", {
+  
+      startingContent: homeStartingContent,
+  
+      posts: posts
+  
+      });
+  
+  })
 });
 app.get("/about", function (req, res) {
-  res.render("about", { content: aboutContent });
+  res.render("about", { aboutContent: aboutContent });
 });
 app.get("/contact", function (req, res) {
-  res.render("contact", { content: contactContent });
+  res.render("contact", { contactContent: contactContent });
 });
 app.get("/compose", function (req, res) {
+ 
   res.render("compose");
 });
 
 app.post("/compose", function (req, res) {
-  const post = {
+ 
+  const post = new Post ({
+
     title: req.body.postTitle,
-    content: req.body.postBody,
-  };
-  arr.push(post);
-  res.redirect("/");
+ 
+    content: req.body.postBody
+ 
+  });
+  post.save(function(err){
+
+    if (!err){
+ 
+      res.redirect("/");
+ 
+    }
+ 
+  });
+
 });
 
-app.get("/post/:postName", function (req, res) {
-  const requestedTitle = _.lowerCase(req.params.postName);
-  arr.forEach(function (post) {
-    const title = post.title;
-    const storedTitle = _.lowerCase(post.title);
-    if (requestedTitle === storedTitle) {
-      res.render("post", { title: post.title, content: post.content });
-    }
-  });
+
+
+app.get("/post/:postId", function (req, res) {
+  
+  const requestedPostId = req.params.postId;
+  Post.findOne({_id: requestedPostId}, function(err, post){ 
+      res.render("post", {
+
+     title: post.title,
+
+     content: post.content
+
+   });
+  })
 });
 
 app.listen(port, function () {
